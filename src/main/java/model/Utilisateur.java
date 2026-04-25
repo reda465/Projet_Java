a452e844c8256ad2f18c4af143f08e7bf806f842
Reda <zakariareda981@gmail.com>
2026-04-25 14:23:37 +0100

    ajustement

--- src/main/java/model/Utilisateur.java
+++ src/main/java/model/Utilisateur.java
@@ -1,4 +1,5 @@
 package model;
+import model.enums.*;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,16 +12,12 @@ import lombok.Setter;
 @AllArgsConstructor
 
 public class Utilisateur {
-
-    private int id;
-    private String nom;
-    private String prenom;
+    private int idUtilisateur;
+    private String nomComplet;
     private String numeroTelephone;
     private String motDePasse;
-    private boolean enLigne;
-
-    // Méthode utile
-    public String getNomComplet() {
-        return prenom + " " + nom;
-    }
+    private java.time.LocalDateTime dateInscription;
+    private java.time.LocalDateTime derniereConnexion;
+    private StatutUtilisateur Status;
+    private String photoProfil;
 }
