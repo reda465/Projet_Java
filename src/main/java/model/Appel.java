8d19e9f9adaced22609ecded60048bdda6460e9c
SAMIRAOUIMING <ouimingsamira@gmail.com>
2026-04-29 14:38:18 +0100

    samiracommit

--- src/main/java/model/Appel.java
+++ src/main/java/model/Appel.java
@@ -7,13 +7,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class Appel {
-    enum TypeAppel {audio,video}
-    enum Status{manque, accepte, refuse, en_cours}
     private int idAppel;
     private int idAppelant;
     private int idConversation;
     private java.time.LocalDateTime dateAppel;
-    private TypeAppel type_appel;
     private int dureeSecondes;
-    private TypeAppel statut;
+    private TypeAppel TypeAppel;
+    private StatutAppel statut;       // manque, accepte, refuse, en_cours
 }
