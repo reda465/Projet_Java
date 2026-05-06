eb8e558b1f8b9c4006be1bbbb3a352ee1cae0c99
Anahir Maryam <maryamanahir6@gmail.com>
2026-05-06 15:56:36 +0200

    test contact

--- src/main/java/javafx/login.java
+++ src/main/java/javafx/login.java
@@ -25,7 +25,7 @@ public class login extends Application implements EcouteurClient {
         stage.setTitle("WhatsApp - Login");
         stage.show();
         ClientHandlerAuth.getInstance()
-                .connecterAuServeur("10.100.106.228", 5000, this);
+                .connecterAuServeur("10.226.120.18", 5000, this);
     }
     public Scene creerScene(Stage stage) {
         String fs = fieldStyle();
