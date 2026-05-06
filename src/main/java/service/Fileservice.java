7dc9189c7a66563627fda1880c40641e819af53b
HANANE <hananeelalaoui@gmail.com>
2026-05-06 12:33:02 +0100

    aa

--- src/main/java/service/Fileservice.java
+++ src/main/java/service/Fileservice.java
@@ -3,31 +3,31 @@ package service;
 import Serveur.Protocol;
 import client.ClientReseau;
 import network.Packet;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.util.Base64;
 
-    public class Fileservice {
+public class FileService {
 
-        private ClientReseau clientReseau;
+    private ClientReseau clientReseau;
 
-
-        public Fileservice(ClientReseau clientReseau) {
-            this.clientReseau = clientReseau;
-        }
-        public void envoyerFichier(String telDest, File file) {
-            try {
-                byte[] bytes = Files.readAllBytes(file.toPath());
-                String base64 = Base64.getEncoder().encodeToString(bytes);
-
-                String data = telDest + "|" + file.getName() + "|" + base64;
-                clientReseau.envoyer(new Packet(Protocol.FILE_SEND, data));
-
-                System.out.println("[FILE] fichier envoyé au serveur : " + file.getName());
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    public FileService(ClientReseau clientReseau) {
+        this.clientReseau = clientReseau;
     }
 
+    public void envoyerFichier(String telDest, File file) {
+        try {
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            String base64 = Base64.getEncoder().encodeToString(bytes);
+
+            String data = telDest + "|" + file.getName() + "|" + base64;
+            clientReseau.envoyer(new Packet(Protocol.FILE_SEND, data));
+
+            System.out.println("[FILE] fichier envoyé au serveur : " + file.getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
