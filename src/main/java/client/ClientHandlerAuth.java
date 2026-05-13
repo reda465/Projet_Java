cb0e7f5c2e0f0da10c7cf677fd0947924d13a6fa
SAMIRAOUIMING <ouimingsamira@gmail.com>
2026-05-13 21:32:34 +0100

    avant changement

--- src/main/java/client/ClientHandlerAuth.java
+++ src/main/java/client/ClientHandlerAuth.java
@@ -232,4 +232,10 @@ public class ClientHandlerAuth {
         }
         return true;
     }
+    //pour effectuer la liaisoon entre disscusion et sign up
+    public void setEcouteur(EcouteurClient ecouteur) {
+        if (clientReseau != null) {
+            clientReseau.setEcouteur(ecouteur);
+        }
+    }
 }
